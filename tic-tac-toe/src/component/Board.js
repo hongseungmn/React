c4c6cd874a8f0493cc2aed0f65209b8e779876d8
Hongseongmn <sem50000@naver.com>
2024-03-11 12:33:18 +0900

    0311_tic-tac-toe마무리

--- tic-tac-toe/src/component/Board.js
+++ tic-tac-toe/src/component/Board.js
@@ -1,13 +1,10 @@
 import { useState } from "react";
 import Square from "./Square";
 
-export default function Board() {
-
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+function Board({xIsNext, squares, onPlay}) {
 
   function handleClick(i) {
-    if(squares[i] || calculateWinner(squares)) { //이미 값이 채워진 경우 or 승자가 정해진 경우 return
+    if(calculateWinner(squares) || squares[i]) { //이미 값이 채워진 경우 or 승자가 정해진 경우 return
       return;
     }
     const nextSquares = squares.slice();
@@ -17,30 +14,7 @@ export default function Board() {
     else {
       nextSquares[i] = 'O';
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
-  }
-
-  function calculateWinner(squares) {
-    const lines = [ //승자가 되는 라인
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-    
-    for (let i=0; i<lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) { //정답을 순회하면서 해당 라인이 존재하는지 확인 후 만약 존재한다면 해당 값(O or X)를 리턴
-        return squares[a];
-      }
-
-    }
-    return null;
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -70,6 +44,71 @@ export default function Board() {
       <Square value={squares[8]} onSquareClick={()=> handleClick(8)}/>
     </div>
   </>)
+}
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextSquares);
+    setCurrentMove(nextHistory.length - 1)
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if(move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    )
+  })
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  )
+}
+
+function calculateWinner(squares) {
+  const lines = [ //승자가 되는 라인
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  
+  for (let i=0; i<lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) { //정답을 순회하면서 해당 라인이 존재하는지 확인 후 만약 존재한다면 해당 값(O or X)를 리턴
+      return squares[a];
+    }
+
+  }
+  return null;
 }
 
 /*
