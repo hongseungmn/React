65a86bd34f5b9b56ea8620707c36e9274e4898f1
Hongseongmn <sem50000@naver.com>
2024-03-14 14:47:03 +0900

    0314_edub_헤더 마무리

--- edub-proto/src/components/Header.js
+++ edub-proto/src/components/Header.js
@@ -7,11 +7,19 @@ export default function Header() {
     <div id="header"> 
       <div className='header_l'>
         <div className="logo">
-          <h1 className="logo_img">
+          <h4 className="logo_img">
             <img src="/codewalks_logo.png" alt="로고"/>
-          </h1>
+          </h4>
         </div>
         <Menubar/>
+      </div>
+      <div className='header_r'>
+        <div className='top-right-btn'>
+          <span className="material-symbols-outlined">library_add</span>
+          라이브러리 추가
+        </div>
+        <div className='top-right-btn'><span class="material-icons">dark_mode</span></div>
+        <div className='top-right-btn'><span class="material-icons">light_mode</span></div>
       </div>
     </div>
   </>)
--- edub-proto/src/index.js
+++ edub-proto/src/index.js
@@ -6,6 +6,12 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css"/>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com"/>
+      <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700&display=swap" rel="stylesheet"/>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"/>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>	
     <App />
   </React.StrictMode>
 );
