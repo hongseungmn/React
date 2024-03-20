d832ce5e4c5580420ae5454274018b3a3a9e983b
홍성민 <79087697+hongseungmn@users.noreply.github.com>
2024-03-20 20:12:59 +0900

    0320_비디오파트 시작

--- zoom/src/public/js/app.js
+++ zoom/src/public/js/app.js
@@ -1,131 +1,21 @@
-/*
-const messageList  = document.querySelector("ul");
-const nickForm = document.querySelector("#nick");
-const messageForm = document.querySelector("#message");
-const socket = new WebSocket(`ws://${window.location.host}`);
-
-
-const openSocket = () => {
-  console.log("Connected to Server ✅");
-};
-
-const messageSocket = (message) => {
-  const li = document.createElement("li");
-  li.innerHTML = message.data;
-  messageList.append(li);
-};
-
-const closeSocket = () => {
-  console.log("Disconnected from the Server ❌");
-};
-
-const makeMessage = (type, payload) => {
-  const msg = {type, payload};
-  return JSON.stringify(msg);
-};
-
-const handleSubmit = (event) => {
-  event.preventDefault();
-  const input = messageForm.querySelector("input");
-  socket.send(makeMessage("new_message", input.value));
-  const li = document.createElement("li");
-  li.innerHTML = `You : ${input.value}`;
-  messageList.append(li);
-  input.value = "";
-};
-
-const handleNickSubmit = (event) => {
-  event.preventDefault();
-  const input = nickForm.querySelector("input");
-  socket.send(makeMessage("nickname",input.value));
-};
-
-socket.addEventListener("open", openSocket);
-socket.addEventListener("message", messageSocket);
-socket.addEventListener("close", closeSocket);
-
-messageForm.addEventListener("submit", handleSubmit);
-nickForm.addEventListener("submit", handleNickSubmit);
-*/
-
-
 const socket = io();
 
-const welcome = document.getElementById("welcome");
-const form = welcome.querySelector("form");
-const room = document.getElementById("room");
+const myFace = document.getElementById("myFace");
 
-room.hidden = true;
-let roomName;
+let myStream;
 
-const handleMessageSubmit = (event) => {
-  event.preventDefault();
-  const input = room.querySelector("#msg input");
-  const value = input.value;
-  socket.emit("new_message", input.value, roomName, ()=> {
-    addMessage(`You: ${value}`);
-  });
-  input.value = "";
-};
-
-const handleNicknameSubmit = (event) => {
-  event.preventDefault();
-  const input = room.querySelector("#name input");
-  socket.emit("nickname", input.value);
-};
-
-const showRoom = () => {
-  welcome.hidden = true;
-  room.hidden = false;
-  const h3 = room.querySelector("h3");
-  h3.innerText= `Room ${roomName}`;
-  const msgForm = room.querySelector("#msg");
-  const nameForm = room.querySelector("#name");
-  msgForm.addEventListener("submit", handleMessageSubmit);
-  nameForm.addEventListener("submit", handleNicknameSubmit);
-}
-
-const addMessage = (message) => {
-  const ul = room.querySelector("ul");
-  const li = document.createElement("li");
-  li.innerText = message;
-  ul.appendChild(li);
-};
-
-const handleRoomSubmit = (event) => {
-  event.preventDefault();
-  const input = form.querySelector("input");
-  socket.emit("enter_room", input.value, showRoom);
-  roomName = input.value;
-  input.value = "";
-};
-
-
-socket.on("welcome", (user, newCount) => {
-  const h3 = room.querySelector("h3");
-  h3.innerText= `Room ${roomName} (${newCount})`;
-  addMessage(`${user} Joined !`);
-});
-
-socket.on("bye", (user, newCount) => {
-  const h3 = room.querySelector("h3");
-  h3.innerText= `Room ${roomName} (${newCount})`;
-  addMessage(`${user} left !!`);
-});
-
-socket.on("new_message", addMessage);
-
-socket.on("room_change", (rooms) => {
-  const roomList = welcome.querySelector("ul");
-  roomList.innerHTML = "";
-  if(rooms.length === 0) {
-    return;
+const getMedia = async () => {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia(
+      {
+        audio: true,
+        video: true,
+      }
+    );
+    myFace.srcObject = myStream;
+  } catch(e) {
+    console.log(e);
   }
-  rooms.forEach(room => {
-    const li = document.createElement("li");
-    li.innerText = room;
-    roomList.append(room);
-  })
-});
+};
 
-form.addEventListener("submit", handleRoomSubmit);
+getMedia();
