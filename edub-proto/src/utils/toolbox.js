d62cf97f244b35aa7075fd4a6d1ed391a8ec7220
홍성민 <79087697+hongseungmn@users.noreply.github.com>
2024-04-16 09:16:47 +0900

    0416_에듀비 툴박스

--- edub-proto/src/utils/toolbox.js
+++ edub-proto/src/utils/toolbox.js
@@ -1,8 +1,78 @@
 import { IO } from "./js/IO"
 
-export let MY_TOOLBOX =
-  '<xml xmlns="https://developers.google.com/blockly/xml" id="toolbox" class="toolbox" style="display:none">';
-MY_TOOLBOX +=
-  ` <category name="TEST" colour="%{BKY_CAT_VAR}" custom="VARIABLE"></category>`;
-MY_TOOLBOX += IO;
-MY_TOOLBOX += "</xml>";
+export let MY_TOOLBOX = {
+  "kind": "categoryToolbox",
+  "contents": [
+    {
+      "kind": "category",
+      "name": "Variables",
+      "custom": "VARIABLE"
+    },
+    {
+      "kind": "category",
+      "name": "IO",
+      "contents": [
+        {
+          "kind": "block",
+          "type": "io_bk1",
+          "blockxml": `
+          <block type="io_bk1">
+          <value name="content1">
+            <shadow type="indata">
+              <field name="indata1"></field>
+            </shadow>
+          </value>
+        </block>
+        `
+        },
+        {
+          "kind": "block",
+          "blockxml": `
+          <block type="io_bk2">
+            <value name="content">
+              <shadow type="indata">
+                <field name="indata1"></field>
+              </shadow>
+              <block type="variables_get">
+                <field name="VAR">var</field>
+              </block>
+            </value>
+          </block>
+          `
+        },
+        {
+          "kind": "block",
+          "blockxml": `
+          <block type="io_bk3">
+            <value name="content1">
+              <shadow type="indata">
+                <field name="indata1">"메시지"</field>
+              </shadow>
+            </value>
+            <value name="content2">
+              <shadow type="indata">
+                <field name="indata1"></field>
+              </shadow>
+              <block type="variables_get">
+                <field name="VAR">var</field>
+              </block>
+            </value>
+          </block>
+          `
+        }
+      ]
+    },
+    {
+      'kind': 'search',
+      'name': 'Search',
+      'contents': [],
+    },
+  ]
+}
+
+
+//   '<xml xmlns="https://developers.google.com/blockly/xml" id="toolbox" class="toolbox" style="display:none">';
+// MY_TOOLBOX +=
+//   ` <category name="TEST" colour="%{BKY_CAT_VAR}" custom="VARIABLE"></category>`;
+// MY_TOOLBOX += IO;
+// MY_TOOLBOX += "</xml>";
