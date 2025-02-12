438b89200e6a2d2e29a594a3395c8c2de3f6b71e
홍성민 <79087697+hongseungmn@users.noreply.github.com>
2025-02-12 23:01:35 +0900

    detail 수정

--- mes-test/src/pages/Departure/detail.jsx
+++ mes-test/src/pages/Departure/detail.jsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+
 export default function DepartureDetail() {
+  const {flt} = useParams(); 
+  
+  // useEffect()
+
   return (
     <div>
       <p>detailPage</p>
+      <p></p>
     </div>
   );
 }
